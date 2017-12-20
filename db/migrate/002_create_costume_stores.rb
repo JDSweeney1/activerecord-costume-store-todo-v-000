7d13def618a8d6264428f6fb0d9d10fbab3294f6
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-20 14:21:02 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :costume_inventory
       t.sting :employee_count
-      t.boolean :still_in_business
+      #t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
