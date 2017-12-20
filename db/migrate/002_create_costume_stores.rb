9ef83ac1d37291577cab60d25f7b5c55c2f41464
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-20 14:16:09 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    creat_table :costumestores do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.integer :employee_count
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
 end
