f8111c98dcffb4e7e55956b9f34cca0646205354
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-20 14:08:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,1 +1,11 @@
 class Costumes < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :price
+      t.string :image_url
+      t.string :size
+      t.timestamp
+    end
+  end
+end
